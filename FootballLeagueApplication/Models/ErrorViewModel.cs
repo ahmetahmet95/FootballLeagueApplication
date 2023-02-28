755e45d5914e0fbe85e55a13fc5901b0da245335
ahmetahmet95 <ahmed061095@gmail.com>
2023-02-28 05:31:48 +0200

    Add project files.

---
+++ FootballLeagueApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballLeagueApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
